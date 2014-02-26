812ebcea113eda5a5445db3eb2082a793bd7d1e5
blackH <blackh@blackJ.(none)>
2014-02-26 15:39:40 +0530

    FMT

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -6,63 +6,64 @@ package raft
 import (
 	"cluster"
 	"encoding/json"
-	"io/ioutil"
-	"path/filepath"
-	"time"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strconv"
 	"sync"
-	"os"
-
+	"time"
 )
 
 //Defining All Stat of the Raft
 const (
-	Leader    = 1
-	Candidate = 2
-	Follower  = 3
-	HearBeatTimeout =50 //this is the interval by which leader will broadcast the HB
+	Leader          = 1
+	Candidate       = 2
+	Follower        = 3
+	HearBeatTimeout = 50 //this is the interval by which leader will broadcast the HB
 )
 
 //Main Raft Class to manage all properties of a raft instanse
 type rafTclass struct {
-	currentTerm            int //Current term of server
-	termMtx	sync.RWMutex //Mutex For term
-	RoleMtx sync.RWMutex; //Mutex for State of server
-	startMtx sync.RWMutex; //mutext for Start
-	votedFor               int //whom to voted last
-	totalvote              int //total vote recived when it is candidate
-	myID                   int // ID of sever
+	currentTerm            int          //Current term of server
+	termMtx                sync.RWMutex //Mutex For term
+	RoleMtx                sync.RWMutex //Mutex for State of server
+	startMtx               sync.RWMutex //mutext for Start
+	votedFor               int          //whom to voted last
+	totalvote              int          //total vote recived when it is candidate
+	myID                   int          // ID of sever
 	LastComm               time.Time
 	currentState           int //State Menas Leader or folllwer or candidate
 	currentLeader          int // who is my leader
 	HeartBeatTime          int
-	ElectionTimeout        int// What is the election timeout for me
+	ElectionTimeout        int // What is the election timeout for me
 	electionTimeoutchannel chan bool
-	started                bool //is server running
+	started                bool              //is server running
 	servreEntity           cluster.ServerObj //Servre cluster object
-	LogFilePATH            string //path to store log files
-	TermFilePath           string //path to store term in Disk
+	LogFilePATH            string            //path to store log files
+	TermFilePath           string            //path to store term in Disk
 }
+
 //Some structure to map with JSON CONFIG
 type timeouts struct {
 	ID             int
 	ElctionTimeout int
 }
 type logFile struct {
-	ID             int
+	ID       int
 	Filename string
 }
 type termFile struct {
-	ID             int
+	ID       int
 	Filename string
 }
+
 //to get raftConfig from the Disk
 type raftConfig struct {
-	Servers  []cluster.ServerConf 	`json:"Servers"`
-	Timeouts []timeouts           	`json:"Timeouts"`
-	LogFile  []logFile              `json:"LogFile"`
-	TermFile []termFile      		`json:"TermFile"`
+	Servers  []cluster.ServerConf `json:"Servers"`
+	Timeouts []timeouts           `json:"Timeouts"`
+	LogFile  []logFile            `json:"LogFile"`
+	TermFile []termFile           `json:"TermFile"`
 }
 
 //Main Raft Interface that is accessible out side the package
@@ -72,25 +73,29 @@ type Raft interface {
 	Stop()
 	Start()
 }
+
 //Update state of the serevr
-func (r *rafTclass) updateState(stat int){
-	r.RoleMtx.Lock();
-	r.currentState=stat;
-	r.RoleMtx.Unlock();
+func (r *rafTclass) updateState(stat int) {
+	r.RoleMtx.Lock()
+	r.currentState = stat
+	r.RoleMtx.Unlock()
 }
+
 //read current state
-func (r *rafTclass) getState()int{
+func (r *rafTclass) getState() int {
 	r.RoleMtx.RLock()
-	stat:=r.currentState
+	stat := r.currentState
 	r.RoleMtx.RUnlock()
-	return stat;
+	return stat
 }
+
 //to stop or kill the serevr
 func (r *rafTclass) Stop() {
 	r.updateStarted(false)
-	r.updateState(Follower);
-	writeTermFromFile(r.TermFilePath,r.getTerm())
+	r.updateState(Follower)
+	writeTermFromFile(r.TermFilePath, r.getTerm())
 }
+
 // to start or wakeup killed server
 func (RaftObj *rafTclass) Start() {
 	RaftObj.updateStarted(true)
@@ -113,58 +118,64 @@ func (RaftObj *rafTclass) Start() {
 	}()
 
 }
+
 // check that am i the leader
-func (r *rafTclass) IsLeader()bool{
-	r.RoleMtx.RLock();
-	term:=r.currentState
-	r.RoleMtx.RUnlock();
+func (r *rafTclass) IsLeader() bool {
+	r.RoleMtx.RLock()
+	term := r.currentState
+	r.RoleMtx.RUnlock()
 	//fmt.Println(term)
-	if term!=Leader {
+	if term != Leader {
 		return false
 	}
-	return true;
+	return true
 }
+
 //get current term of the server
-func (r *rafTclass) Term()int{
-	r.termMtx.RLock();
-	term:=r.currentTerm
-	r.termMtx.RUnlock();
-	return term;
+func (r *rafTclass) Term() int {
+	r.termMtx.RLock()
+	term := r.currentTerm
+	r.termMtx.RUnlock()
+	return term
 }
+
 //Update term of the server
-func (r *rafTclass) updateTerm(term int){
-	r.termMtx.Lock();
+func (r *rafTclass) updateTerm(term int) {
+	r.termMtx.Lock()
 	//fmt.Println(r.TermFilePath)
-	writeTermFromFile(r.TermFilePath,term);
-	r.currentTerm=term;
-	r.termMtx.Unlock();
+	writeTermFromFile(r.TermFilePath, term)
+	r.currentTerm = term
+	r.termMtx.Unlock()
 }
+
 //Get the term that is used internally
-func (r *rafTclass) getTerm()int{
-	r.termMtx.RLock();
-	term:=r.currentTerm
-	r.termMtx.RUnlock();
-	return term;
+func (r *rafTclass) getTerm() int {
+	r.termMtx.RLock()
+	term := r.currentTerm
+	r.termMtx.RUnlock()
+	return term
 }
+
 //update start variable that kill or wakeup serevr
-func (r *rafTclass) updateStarted(start bool){
-	r.startMtx.Lock();
-	r.started=start;
-	r.startMtx.Unlock();
+func (r *rafTclass) updateStarted(start bool) {
+	r.startMtx.Lock()
+	r.started = start
+	r.startMtx.Unlock()
 }
+
 //GET start variable that kill or wakeup serevr
-func  (r *rafTclass) getStart() bool{
+func (r *rafTclass) getStart() bool {
 	r.startMtx.RLock()
-	start:=r.started
-	r.startMtx.RUnlock();
-	return start;
+	start := r.started
+	r.startMtx.RUnlock()
+	return start
 
 }
 
 //Miscellaneous Function of Raft
 
 //RAFT initializer
-func NewRaft(id int, path string) *Raft{
+func NewRaft(id int, path string) *Raft {
 	//Intialising Object Properties
 	RaftObj := new(rafTclass)
 	cofigpath, _ := filepath.Abs(path)
@@ -174,10 +185,10 @@ func NewRaft(id int, path string) *Raft{
 		if alldetails.Timeouts[timeOu].ID == id {
 			RaftObj.ElectionTimeout = alldetails.Timeouts[timeOu].ElctionTimeout
 		}
-		if alldetails.LogFile[timeOu].ID == id{
+		if alldetails.LogFile[timeOu].ID == id {
 			RaftObj.LogFilePATH = alldetails.LogFile[timeOu].Filename
 		}
-		if alldetails.TermFile[timeOu].ID == id{
+		if alldetails.TermFile[timeOu].ID == id {
 			RaftObj.TermFilePath = alldetails.TermFile[timeOu].Filename
 		}
 	}
@@ -198,20 +209,20 @@ func NewRaft(id int, path string) *Raft{
 	//go RaftObj.timeout();
 	fmt.Println(strconv.Itoa(RaftObj.myID) + " - " + strconv.Itoa(RaftObj.currentTerm) + " Start -> Follower")
 	RaftObj.Start()
-	var x Raft;
-	x=&*RaftObj
+	var x Raft
+	x = &*RaftObj
 	return &x
 }
 
 //Evaluate that is it ok to send vote if it is ok then return TRUE
 func (RaftObj *rafTclass) sendVote(req cluster.VoteReq) bool {
 	//If older request then current term then REFUSE
-	if req.Term <= RaftObj.getTerm(){
+	if req.Term <= RaftObj.getTerm() {
 		//fmt.Println("got Negative Old term res term"+strconv.Itoa(req.Term)+" MY term "+strconv.Itoa(RaftObj.currentTerm))
 		return false
 	}
 	//if already voted then no need to vote-
-	if RaftObj.votedFor != req.IdCandidate && RaftObj.votedFor != 0 && req.Term <=  RaftObj.getTerm() {
+	if RaftObj.votedFor != req.IdCandidate && RaftObj.votedFor != 0 && req.Term <= RaftObj.getTerm() {
 		//fmt.Println("got Negative Already Voted res term"+strconv.Itoa(req.Term)+" MY term "+strconv.Itoa(RaftObj.currentTerm))
 		return false
 	}
@@ -227,7 +238,7 @@ func (RaftObj *rafTclass) performasFollower() {
 	if RaftObj.getState() == Follower {
 		RaftObj.totalvote = 0
 		select {
-			//check for incoming message
+		//check for incoming message
 		case x := <-RaftObj.servreEntity.Inbox():
 			var msg cluster.Envelope
 			msg = *x
@@ -255,10 +266,10 @@ func (RaftObj *rafTclass) performasFollower() {
 		case <-time.After(time.Duration(RaftObj.ElectionTimeout) * time.Millisecond):
 			RaftObj.votedFor = RaftObj.myID
 			RaftObj.totalvote = 1
-			RaftObj.updateTerm(RaftObj.getTerm()+ 1)
+			RaftObj.updateTerm(RaftObj.getTerm() + 1)
 			RaftObj.updateState(Candidate)
 			//Broadcast the request for vote
-		RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: cluster.BRODCAST, MsgId: 90, Msg: cluster.VoteReq{Term: RaftObj.currentTerm, IdCandidate: RaftObj.myID}}
+			RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: cluster.BRODCAST, MsgId: 90, Msg: cluster.VoteReq{Term: RaftObj.currentTerm, IdCandidate: RaftObj.myID}}
 			fmt.Println(strconv.Itoa(RaftObj.myID) + " - " + strconv.Itoa(RaftObj.currentTerm) + "Timeout Follower->Candidate")
 		}
 
@@ -286,7 +297,7 @@ func (RaftObj *rafTclass) perfomeasCandidate() {
 				//If gets any voteReqThen it will remove refuse all to give vote because it voted for himself
 			case cluster.VoteReq:
 				req := msg.Msg.(cluster.VoteReq)
-			RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: req.IdCandidate, MsgId: 90, Msg: cluster.VoteRespose{Term: msg.Msg.(cluster.VoteReq).Term, VoteResult: false}}
+				RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: req.IdCandidate, MsgId: 90, Msg: cluster.VoteRespose{Term: msg.Msg.(cluster.VoteReq).Term, VoteResult: false}}
 
 			case cluster.VoteRespose:
 				res := msg.Msg.(cluster.VoteRespose)
@@ -311,7 +322,7 @@ func (RaftObj *rafTclass) perfomeasCandidate() {
 
 			} else {
 				fmt.Println(strconv.Itoa(RaftObj.myID) + " -  Term " + strconv.Itoa(RaftObj.currentTerm) + " Relection Not Having Enough Votes Total Votes " + strconv.Itoa(RaftObj.totalvote))
-				RaftObj.updateTerm(RaftObj.getTerm()+ 1)
+				RaftObj.updateTerm(RaftObj.getTerm() + 1)
 				RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: cluster.BRODCAST, MsgId: 90, Msg: cluster.VoteReq{Term: RaftObj.currentTerm, IdCandidate: RaftObj.myID}}
 				RaftObj.votedFor = RaftObj.myID
 				RaftObj.currentLeader = 0
@@ -330,7 +341,7 @@ func (RaftObj *rafTclass) perfomeasLeader() {
 		//Broadcast Heat Beat to EveryOne
 		RaftObj.servreEntity.Outbox() <- &cluster.Envelope{Pid: cluster.BRODCAST, MsgId: 90, Msg: cluster.HeartBeat{Term: RaftObj.currentTerm, LeaderId: RaftObj.myID}}
 		select {
-			//if any one is Leader with Higher term Then step Back to follower
+		//if any one is Leader with Higher term Then step Back to follower
 		case x := <-RaftObj.servreEntity.Inbox():
 			var msg cluster.Envelope
 			msg = *x
@@ -356,30 +367,32 @@ func getALLCOnfig(path string) raftConfig {
 	//	fmt.Println(raftcon);
 	return raftcon
 }
+
 //read current save term in disk
-func readTermFromFile(path string)int{
-	fil,er:=os.OpenFile(path,os.O_CREATE|os.O_WRONLY , 0660)
-	if er!=nil{
-		return 1;
+func readTermFromFile(path string) int {
+	fil, er := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0660)
+	if er != nil {
+		return 1
 
 	}
-	var b []byte;
-	fil.Read(b);
-	x,_:=strconv.Atoi(string(b))
-	fil.Close();
+	var b []byte
+	fil.Read(b)
+	x, _ := strconv.Atoi(string(b))
+	fil.Close()
 	return x
 }
+
 //Write current term to disk.
-func writeTermFromFile(path string,term int){
+func writeTermFromFile(path string, term int) {
 	//fmt.Println(path)
-	x,_:=filepath.Abs(".");
-	x=x+"/"+path
+	x, _ := filepath.Abs(".")
+	x = x + "/" + path
 	//fmt.Println(x);
-	fil,er:=os.OpenFile(x,os.O_CREATE|os.O_WRONLY |os.O_TRUNC, 0660)
-	if er!=nil {
+	fil, er := os.OpenFile(x, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0660)
+	if er != nil {
 		//panic(path)
 		panic(er)
 	}
 	fil.Write([]byte(strconv.Itoa(term)))
-	fil.Close();
+	fil.Close()
 }
--- src/raft/raft_test.go
+++ src/raft/raft_test.go
@@ -1,12 +1,13 @@
 package raft
 
 import (
+	"log"
 	"testing"
 	"time"
-	"log"
 	//"strconv"
 	//"fmt"
 )
+
 func TestLEADERELCETION(t *testing.T) {
 	all := make(map[int]Raft)
 	//Initialize all of the Raft Objects Creating total 10 objects
@@ -15,71 +16,70 @@ func TestLEADERELCETION(t *testing.T) {
 	}
 	time.Sleep(1 * time.Second)
 	//Counting total leaders
-	leaderCount:=0;
-	leaderindex:=0;
+	leaderCount := 0
+	leaderindex := 0
 	for i := 1; i < 11; i++ {
 		if all[i].IsLeader() {
-			leaderCount+=1;
-			leaderindex=i;
+			leaderCount += 1
+			leaderindex = i
 		}
 
 	}
 	//if total leader is one then it is ok becasuse there can be atmost one leader
-	if leaderCount==1 {
+	if leaderCount == 1 {
 		log.Println("Only One Leader Found Its ")
-	}else{
+	} else {
 		log.Panic("No Lader Found or moer then one leader found")
 	}
 	//Kill 3 Servers that is not leader
-	kill:=0;
+	kill := 0
 	//Three Follower has been stopped
 	for i := 1; i < 11; i++ {
 		if !all[i].IsLeader() {
-			all[i].Stop();
-			kill++;
+			all[i].Stop()
+			kill++
 		}
-		if kill>=3{
-			break;
+		if kill >= 3 {
+			break
 		}
 	}
 	//and also kill one leader and now total running servers are 6
-	all[leaderindex].Stop();
-	time.Sleep(2*time.Second)
+	all[leaderindex].Stop()
+	time.Sleep(2 * time.Second)
 	//Count Again How many leaders are there
-	leaderCount=0;
+	leaderCount = 0
 	for i := 1; i < 11; i++ {
 		if all[i].IsLeader() {
-			leaderCount+=1;
+			leaderCount += 1
 			log.Println(i)
-			leaderindex=i;
+			leaderindex = i
 		}
 
 	}
 	//if there is only one leader then its ok
-	if leaderCount==1 {
+	if leaderCount == 1 {
 		log.Println("Only One Leader Found Minority Failure  ")
-	}else{
+	} else {
 		log.Panic("Leader Found More then One or None Fail")
 	}
 	//Now stop leader also that will lead to 5 server cluster and that is Majority Failure
-	all[leaderindex].Stop();
-	time.Sleep(2*time.Second)
+	all[leaderindex].Stop()
+	time.Sleep(2 * time.Second)
 	//Now count all leader
-	leaderCount=0;
+	leaderCount = 0
 	for i := 1; i < 11; i++ {
 		if all[i].IsLeader() {
-			leaderCount+=1;
+			leaderCount += 1
 			log.Println(i)
-			leaderindex=i;
+			leaderindex = i
 		}
 
 	}
 	//Here we should not have any leader because minority cant choose leader
-	if leaderCount==0 {
+	if leaderCount == 0 {
 		log.Println("No Lader Found in Majority Failure as it should not be")
-	}else{
+	} else {
 		log.Panic("Something is wrong how can we get a leder in majority failure")
 	}
-
 
 }
