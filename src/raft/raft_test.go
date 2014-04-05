a7adfec747de3d05fe5f43613c5646bfe055e20c
Yogender Solanki <yogendersolanki91@gmail.com>
2014-04-05 09:34:34 -0700

    Update raft_test.go

--- src/raft/raft_test.go
+++ src/raft/raft_test.go
@@ -4,13 +4,13 @@ import (
 	"net/rpc"
 	"testing"
 	//"os/exec"
-	"log"
-	"os/exec"
-	"strconv"
-	"time"
-	"reflect"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"log"
+	"os/exec"
+	"reflect"
+	"strconv"
+	"time"
 )
 
 func checkleaders(count *int, index *int) {
@@ -100,29 +100,29 @@ func TestBasicLogReplication(t *testing.T) {
 
 		cmd := exec.Command("C:\\CloudSrc\\src\\RaftDummy.exe", "-id", strconv.Itoa(key+1), "-dbgport", strconv.Itoa(dbg[key]))
 		cmd.Start()
-		log.Println("Server ",key,"Started")
+		log.Println("Server ", key, "Started")
 
 	}
-	time.Sleep(15*time.Second)
+	time.Sleep(15 * time.Second)
 	//Basic Test-Alsmost One leader at a time
 
 	log.Println("All server started.Running Basic Replication")
 	for _, value := range dbg {
-		log.Println("Killed",value)
+		log.Println("Killed", value)
 		Kill(value)
 	}
-	time.Sleep(10*time.Second)
-	if !check(totalClient){
+	time.Sleep(10 * time.Second)
+	if !check(totalClient) {
 		t.Error("not passed unmached entry found")
 	}
-	time.Sleep(5*time.Second)
+	time.Sleep(5 * time.Second)
 	//log.Println("Running minority failure test")
 	//MinorityFailure(t)
 	//log.Println("Running Majority failure test")
 	//MajorityFailure(t)
 
 }
-func TestCrashFollower(t *testing.T){
+func TestCrashFollower(t *testing.T) {
 	baseDbgPort := 1910
 	//var dbg [totalClient]int{19910, 19911, 19912, 19913, 19914, 19915, 19916, 19917, 19918, 19919}
 	log.Println("Starting total 5 server as given in the config file...")
@@ -135,61 +135,57 @@ func TestCrashFollower(t *testing.T){
 	for key, _ := range dbg {
 		cmd := exec.Command("C:\\CloudSrc\\src\\RaftDummy.exe", "-id", strconv.Itoa(key+1), "-dbgport", strconv.Itoa(dbg[key]))
 		cmd.Start()
-		log.Println("Server ",key,"Started")
+		log.Println("Server ", key, "Started")
 
 	}
-	time.Sleep(5*time.Second)
+	time.Sleep(5 * time.Second)
 	log.Println("Kiling 4")
- 	Kill(dbg[3])
-	time.Sleep(time.Second*5)
+	Kill(dbg[3])
+	time.Sleep(time.Second * 5)
 	log.Println("Now legged server started..")
 	cmd := exec.Command("C:\\CloudSrc\\src\\RaftDummy.exe", "-id", strconv.Itoa(4), "-dbgport", strconv.Itoa(dbg[3]))
 	cmd.Start()
-	time.Sleep(25*time.Second)
-	for	_, value := range dbg {
-		log.Println("Killed",value)
+	time.Sleep(25 * time.Second)
+	for _, value := range dbg {
+		log.Println("Killed", value)
 		Kill(value)
 	}
-	time.Sleep(5*time.Second)
-	if !check(totalClient){
+	time.Sleep(5 * time.Second)
+	if !check(totalClient) {
 		t.Error("not passed")
 	}
 }
 
-func check (count int)bool{
+func check(count int) bool {
 	var allDB [6]*leveldb.DB
 	var allDBMAP [6]map[string]string
 	var allIterator [6]iterator.Iterator
-	for i:=1;i<=count;i++{
-		allDBMAP[i]=make(map[string]string)
-		allD,err:=leveldb.OpenFile("db_"+strconv.Itoa(i)+".db",nil)
-		if err!=nil{
+	for i := 1; i <= count; i++ {
+		allDBMAP[i] = make(map[string]string)
+		allD, err := leveldb.OpenFile("db_"+strconv.Itoa(i)+".db", nil)
+		if err != nil {
 			panic(err)
-		}else{
-			allDB[i]=allD
-			allIterator[i]=allD.NewIterator(nil,nil)
+		} else {
+			allDB[i] = allD
+			allIterator[i] = allD.NewIterator(nil, nil)
 		}
 	}
 
-
-
-	for k:=1;k<=count ;k++{
+	for k := 1; k <= count; k++ {
 		for allIterator[k].Next() {
 
-			allDBMAP[k][string(allIterator[k].Key())]=string(allIterator[k].Value())
+			allDBMAP[k][string(allIterator[k].Key())] = string(allIterator[k].Value())
 
 		}
 
 	}
-	for k:=1;k<count;k++{
-		if reflect.DeepEqual(allDBMAP[k],allDBMAP[k+1]){
-			log.Println(reflect.DeepEqual(allDBMAP[k],allDBMAP[k+1]),k)
-		}else{
+	for k := 1; k < count; k++ {
+		if reflect.DeepEqual(allDBMAP[k], allDBMAP[k+1]) {
+			log.Println(reflect.DeepEqual(allDBMAP[k], allDBMAP[k+1]), k)
+		} else {
 			return false
 		}
 	}
 	return true
-	
-
 
 }
